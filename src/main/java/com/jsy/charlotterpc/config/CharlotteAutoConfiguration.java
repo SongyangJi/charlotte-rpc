33e7ac09845ee51046098b849145e323e993d09e
SongyangJi <1539242799@qq.com>
2021-09-27 02:00:45 +0800

    rename Charlotte

--- src/main/java/com/jsy/charlotterpc/config/CharlotteAutoConfiguration.java
+++ src/main/java/com/jsy/charlotterpc/config/CharlotteAutoConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
  */
 
 @Configuration
-public class LunaAutoConfiguration {
+public class CharlotteAutoConfiguration {
 
     @Bean
     public LocalRegistry localRegistry() {
